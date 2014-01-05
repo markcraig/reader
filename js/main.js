47652db528db4ad8024fb5398b1f311e4c1ba8f5
Mark Craig <mark.craig@gmail.com>
2014-01-05 17:22:13 +0100

    Placeholder rewrite of index page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2014 Mark Craig
+ */
+
+angular.module('main', ['ngRoute', 'ui.bootstrap'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'partials/empty.html',
+                controller: function($scope) {
+                    $scope.message = 'Nothing to read right now';
+                }
+            })
+    });
