9d750ffe40673f610b351bf7a7507eb49e7a8b7f
Mark Craig <mark.craig@gmail.com>
2014-03-08 10:55:42 +0100

    Display static feed HTML in new layout

--- js/main.js
+++ js/main.js
@@ -6,13 +6,77 @@
  * Copyright 2014 Mark Craig
  */
 
-angular.module('main', ['ngRoute', 'ui.bootstrap'])
+angular.module('main', ['ngResource', 'ngRoute', 'ngSanitize', 'ui.bootstrap'])
+    .factory('Feeds', function ($resource) {
+        return $resource('examples/feeds.json', {}, {
+                get: {
+                    method: 'GET',
+                    isArray: true
+                }
+        });
+    })
+    .factory('FeedLoader', function ($resource) {
+        return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
+            fetch: {
+                method: 'JSONP',
+                params: {
+                    v: '1.0',
+                    callback: 'JSON_CALLBACK'
+                }
+            }
+        });
+    })
+    .service('FeedList', function ($rootScope, Feeds, FeedLoader) {
+        this.get = function () {
+            var feedList, feeds, i;
+
+            //feedList = Feeds.get();
+            feedList = [
+                {
+                    "url": "http://www.jwz.org/blog/feed/",
+                    "title": "jwz"
+                },
+                {
+                    "url": "http://ludopoitou.wordpress.com/feed/",
+                    "title": "Ludo's Sketches"
+                },
+                {
+                    "url": "http://marginnotes2.wordpress.com/feed/",
+                    "title": "Margin Notes 2.0"
+                },
+                {
+                    "url": "https://www.tbray.org/ongoing/ongoing.atom",
+                    "title": "ongoing by Tim Bray"
+                },
+                {
+                    "url": "http://www.schneier.com/blog/atom.xml",
+                    "title": "Schneier on Security"
+                }
+            ]
+
+            feeds = [];
+            for (i = 0; i < feedList.length; i += 1) {
+                FeedLoader.fetch({q: feedList[i].url}, {}, function (data) {
+                    var feed = data.responseData.feed;
+                    console.log(angular.toJson(feed));
+                    feeds.push(feed);
+                });
+            }
+            return feeds;
+        }
+    })
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'partials/empty.html',
-                controller: function($scope) {
-                    $scope.message = 'Nothing to read right now';
+                templateUrl: 'partials/feeds.html',
+                controller: function($scope, $sce, FeedList) {
+                    $scope.feeds = FeedList.get();
+                    $scope.$on('FeedList', function (event, data) {
+                        $scope.feeds = data;
+                    });
+                    $scope.trust = function (html) {
+                        return $sce.trustAsHtml(html);
+                    }
                 }
             })
             .when('/configure', {
